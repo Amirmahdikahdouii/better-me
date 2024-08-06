02d47b9ac7a336cb8efd95262af28f999bcea947
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-08-06 20:41:51 +0330

    New model for saving note for habits added

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, func, Text
 from sqlalchemy.orm import relationship
 from .db import Base, engine
 
@@ -28,9 +28,27 @@ class UserHabit(Base):
     name = Column(String(50))
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     user = relationship("User", back_populates="habits")
+    notes = relationship("HabitNote", back_populates="habit")
+    start_date = Column(Date, default=func.current_date())
 
     def __repr__(self):
         return self.user.user_id
 
     def __str__(self):
         return self.name
+
+
+class HabitNote(Base):
+    __tablename__ = "habit_note"
+
+    id = Column(Integer, primary_key=True, index=True)
+    habit_id = Column(Integer, ForeignKey("habits.id"), nullable=False)
+    habit = relationship("UserHabit", back_populates="notes")
+    title = Column(String(250), nullable=False)
+    note = Column(Text, nullable=True)
+
+    def __repr__(self):
+        return self.habit.name
+
+    def __str__(self):
+        return self.habit.name
